a07e73ca46d64f5061416f2f55711d60fc2669a8
EdsonMSouza <prof.edson.melo@gmail.com>
2020-05-05 22:04:05 -0300

    Continuação de Objetos na ListView

--- aula_2804/app/src/main/java/com/ems/aula_2804/MainActivity.java
+++ aula_2804/app/src/main/java/com/ems/aula_2804/MainActivity.java
@@ -2,12 +2,17 @@ package com.ems.aula_2804;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -48,16 +53,63 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 alunos.add(
-                       new Aluno(
-                               ra.getText().toString(),
-                               nome.getText().toString(),
-                               curso.getText().toString()
-                       )
+                        new Aluno(
+                                ra.getText().toString(),
+                                nome.getText().toString(),
+                                curso.getText().toString()
+                        )
                 );
                 // avisar o adapter que os dados mudaram
                 adapter.notifyDataSetChanged();
+
+                // limpar os campos
+                ra.setText("");
+                nome.setText("");
+                curso.setText("");
+
+                // posicionar o cursor no campo ra
+                ra.requestFocus();
+
+                // fechar o teclado virtual após inserir
+                ((InputMethodManager) MainActivity.this.getSystemService(
+                        Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(
+                        getCurrentFocus().getWindowToken(), 0);
+
             }
         });
 
+        lvAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // (Aluno) é conhecido como CAST ou Conversão de Tipo
+                Aluno aluno = (Aluno) lvAlunos.getItemAtPosition(position);
+
+                Toast.makeText(MainActivity.this,
+                        aluno.getDados(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        lvAlunos.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                // resgatando o objeto que está dentro do ArrayAdpater na ListView
+                Aluno aluno = (Aluno) lvAlunos.getItemAtPosition(position);
+
+                // criar um novo ArrayList para acomodar o objeto
+                ArrayList<Aluno> al = new ArrayList<>();
+
+                // coloca o objeto dentro do ArrayList
+                al.add(aluno);
+
+                Intent it = new Intent(MainActivity.this, Resultado.class);
+
+                // inserir o ArrayList com o objeto dentro da Intent
+                it.putExtra("objAluno", al);
+                startActivity(it);
+
+                return false;
+            }
+        });
     }
 }
--- aula_2804/app/src/main/java/com/ems/aula_2804/Resultado.java
+++ aula_2804/app/src/main/java/com/ems/aula_2804/Resultado.java
@@ -3,12 +3,38 @@ package com.ems.aula_2804;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Resultado extends AppCompatActivity {
+    TextView ra, nome, curso;
+    Button btVoltar;
+    private ArrayList<Aluno> objAluno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_resultado);
+        ra = findViewById(R.id.textViewRa);
+        nome = findViewById(R.id.textViewNome);
+        curso = findViewById(R.id.textViewCurso);
+        btVoltar = findViewById(R.id.btVoltar);
+
+        // recuperar o objeto enviado pela Intent
+        objAluno = (ArrayList<Aluno>) getIntent().getSerializableExtra("objAluno");
+
+        ra.setText(objAluno.get(0).getRa());
+        nome.setText(objAluno.get(0).getNome());
+        curso.setText(objAluno.get(0).getCurso());
+
+        btVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
